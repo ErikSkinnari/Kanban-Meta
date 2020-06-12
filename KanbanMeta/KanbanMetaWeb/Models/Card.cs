d6835fe163ceabaf28342402636016b3fbb99e6c
Filip Jinstrand <filip@jinstrand.se>
2020-06-12 11:59:18 +0200

    Updated IDataManager and DataService

--- KanbanMeta/KanbanMetaWeb/Models/Card.cs
+++ KanbanMeta/KanbanMetaWeb/Models/Card.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace KanbanMetaWeb.Models
 {
     public class Card
     {
-        public string id { get; set; }
-        public string boardId { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public int columnId { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("boardId")]
+        public string BoardId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("columnId")]
+        public int ColumnId { get; set; }
 
         public void MoveCardRight() 
         { 
